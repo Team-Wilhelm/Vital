c115e43085570fdb37168ebc9011b801b6d254ff
Julia Ilasova <1julka1il@gmail.com>
2023-11-14 14:03:21 +0100

    structural skeleton for analytics page

--- Vital-Frontend/src/app/app-routing.module.ts
+++ Vital-Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {AnalyticsComponent} from "./analytics/analytics.component";
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  {path: 'analytics', component: AnalyticsComponent},
 ];
 
 @NgModule({
--- Vital-Frontend/src/app/app.module.ts
+++ Vital-Frontend/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import {NgOptimizedImage} from "@angular/common";
+import { AnalyticsComponent } from './analytics/analytics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    AnalyticsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
