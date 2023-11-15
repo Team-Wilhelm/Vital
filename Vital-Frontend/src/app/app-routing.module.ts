13d42549207e1f37177c132d106e5ae75ea7acce
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-11-15 15:09:17 +0100

    added page for inputting metrics/values, routing with date param not working

--- Vital-Frontend/src/app/app-routing.module.ts
+++ Vital-Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {AddMetricPageComponent} from "./add-metric-page/add-metric-page.component";
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, pathMatch: 'full' },
+  //TODO check routing
+  { path: 'add-metric/:clickedDate', component: AddMetricPageComponent, pathMatch: 'prefix' },
 ];
 
 @NgModule({
--- Vital-Frontend/src/app/app.module.ts
+++ Vital-Frontend/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FullCalendarModule } from '@fullcalendar/angular';
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { AddMetricPageComponent } from './add-metric-page/add-metric-page.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    CalendarComponent,
+    AddMetricPageComponent
   ],
   imports: [
+    FullCalendarModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
