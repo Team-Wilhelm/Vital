163f1d6fe6f93795ec27b0a003792763a7e32bd2
Julia Ilasova <1julka1il@gmail.com>
2023-11-15 13:53:10 +0100

    the desktop view of dashboard now has the correct order again

--- Vital-Frontend/src/app/dashboard/current-cycle/current-cycle.component.ts
+++ Vital-Frontend/src/app/dashboard/current-cycle/current-cycle.component.ts
@@ -6,8 +6,8 @@ import {Component} from '@angular/core';
     <div class="flex justify-center items-center text-center rounded-full bg-pink-300 aspect-square
     w-10
     sm:w-16
-    md:w-24">
-      <p class="text-sm sm:text-lg md:text-2xl">13.11</p>
+    lg:w-24">
+      <p class="text-sm sm:text-lg lg:text-2xl">13.11</p>
     </div>
   `
 })
