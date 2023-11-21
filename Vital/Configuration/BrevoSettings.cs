2691ad65cac7543f654afa69edb9aacafebaa507
Matej Mazúr <matejmazur@outlook.com>
2023-11-21 13:29:07 +0100

    Add Brevo mailing service

---
+++ Vital/Configuration/BrevoSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Vital.Configuration;
+
+public class BrevoSettings
+{
+    public string DisplayName { get; set; }
+    public string From { get; set; }
+    public string ApiKey { get; set; }
+}
---
+++ Vital/Core/Services/Interfaces/IMailService.cs
@@ -0,0 +1,6 @@
+﻿namespace Vital.Core.Services.Interfaces;
+
+public interface IMailService
+{
+    Task<bool> SendAsync(List<string> recipients, string subject, string message, CancellationToken ct = default);
+}
