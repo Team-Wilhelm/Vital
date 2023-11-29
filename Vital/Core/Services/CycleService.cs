e0b9d8ae63180162e63ff5d98dacdc6c139c6a51
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-11-29 15:22:58 +0100

    changed getPredictedDays to have real values, not tested

--- Vital/Core/Services/CycleService.cs
+++ Vital/Core/Services/CycleService.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using Infrastructure.Repository.Interface;
+using Microsoft.AspNetCore.Identity;
 using Models;
 using Models.Days;
 using Models.Dto.Cycle;
+using Models.Identity;
 using Models.Pagination;
 using Vital.Core.Context;
 using Vital.Core.Services.Interfaces;
@@ -15,41 +17,64 @@ public class CycleService : ICycleService
     private readonly ICycleRepository _cycleRepository;
     private readonly IMapper _mapper;
     private readonly CurrentContext _currentContext;
+    private readonly UserManager<ApplicationUser> _userManager;
     private readonly ICalendarDayRepository _calendarDayRepository;
 
     public CycleService(ICycleRepository cycleRepository, ICalendarDayRepository calendarDayRepository, IMapper mapper,
-        CurrentContext currentContext)
+        CurrentContext currentContext, UserManager<ApplicationUser> userManager)
     {
         _cycleRepository = cycleRepository;
         _calendarDayRepository = calendarDayRepository;
         _mapper = mapper;
         _currentContext = currentContext;
+        _userManager = userManager;
     }
 
+    /// <summary>
+    /// Get all cycles paginated for the current user.
+    /// </summary>
+    /// <param name="paginator"></param>
+    /// <returns></returns>
     public async Task<PaginatedList<Cycle>> Get(Paginator paginator)
     {
         return await _cycleRepository.Get(paginator);
     }
 
+    /// <summary>
+    /// Get a cycle by its id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public async Task<Cycle?> GetById(Guid id)
     {
         return await _cycleRepository.GetById(id);
     }
 
+    /// <summary>
+    /// Create a new cycle for the current user.
+    /// </summary>
+    /// <returns></returns>
     public async Task<Cycle> Create()
     {
-        var cycle = new Cycle()
+        var cycle = new Cycle
         {
             StartDate = DateTimeOffset.Now,
-            EndDate = null
+            EndDate = null,
+            Id = Guid.NewGuid(),
+            UserId = _currentContext.UserId!.Value
         };
-        cycle.Id = Guid.NewGuid();
-        cycle.UserId = _currentContext.UserId!.Value;
         await _cycleRepository.Create(cycle);
 
         return cycle;
     }
 
+    /// <summary>
+    /// Update a cycle by its id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    /// <exception cref="NotFoundException"></exception>
     public async Task<Cycle> Update(Guid id, UpdateCycleDto dto)
     {
         var cycle = await _cycleRepository.GetById(id);
@@ -64,6 +89,12 @@ public class CycleService : ICycleService
         return cycle;
     }
 
+    /// <summary>
+    /// Calculates remaining days in current cycle and predicted period days for the next three cycles.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    /// <exception cref="NotFoundException"></exception>
     public async Task<List<DateTimeOffset>> GetPredictedPeriod(Guid userId)
     {
         var currentCycle = await GetCurrentCycle(userId);
@@ -73,28 +104,29 @@ public class CycleService : ICycleService
         }
         
         var predictedPeriodDays = new List<DateTimeOffset>();
+        var cycleLength = _userManager.Users.First(u => u.Id == userId).CycleLength;
+        var periodLength = _userManager.Users.First(u => u.Id == userId).PeriodLength;
+        var cycleStartDay = currentCycle.StartDate;
         
-        // Check if the user is on period and predicted when it should end based on the average period length.
-        var periodDaysPassed = currentCycle.CycleDays.Count(day => day.IsPeriod); 
-        if (periodDaysPassed is < 5 and >= 1) //TODO: Change hardcoded value
+        //get latest period day for current cycle and add predicted days based on period length
+        var latestPeriodDay = currentCycle.CycleDays.Where(d => d.IsPeriod).OrderBy(d => d.Date).Last();
+        var difference = (int)(latestPeriodDay.Date - cycleStartDay).TotalDays;
+        
+        for (var i = 0; i < difference; i++)
         {
-            for (var i = 0; i < 5 - periodDaysPassed; i++)
+            predictedPeriodDays.Add(latestPeriodDay.Date.AddDays(i + 1));
+        }
+        
+        //get predicted period days for the next three cycles
+        for (var i = 1; i < 4; i++)
+        {
+            var futureCycleStartDay = cycleStartDay.Date.AddDays(cycleLength*i);
+            for (var j = 0; j < periodLength; j++)
             {
-                predictedPeriodDays.Add(DateTimeOffset.Now.AddDays(i + 1));
+                predictedPeriodDays.Add(futureCycleStartDay.Date.AddDays(cycleLength + j));
             }
-            return predictedPeriodDays;
         }
         
-        
-        // Otherwise the predicted period is calculated based on the average cycle length after the last period started.
-        // The average cycle length is calculated based on the last 3 cycles.
-        currentCycle.CycleDays = currentCycle.CycleDays.OrderBy(x => x.Date).ToList();
-        for (var i = -2; i < 3; i++)
-        {
-            predictedPeriodDays.Add(currentCycle.CycleDays.Last().Date.AddDays(28 + i));
-        }
-
-        // TODO: change
         return predictedPeriodDays;
     }
 
