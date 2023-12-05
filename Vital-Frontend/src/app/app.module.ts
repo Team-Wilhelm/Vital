29b02828cbe3f74e867c9af8c8da230a9e92cf91
Julia Ilasova <1julka1il@gmail.com>
2023-12-05 12:41:02 +0100

    added application-stat-card.component.ts, in order to make stat cards more responsive for the analytics page

--- Vital-Frontend/src/app/app.module.ts
+++ Vital-Frontend/src/app/app.module.ts
@@ -22,12 +22,13 @@ import {CalendarComponent} from './calendar/calendar.component';
 import {AddMetricPageComponent} from './add-metric-page/add-metric-page.component';
 import {MetricListItemComponent} from "./dashboard/metric-list-item/metric-list-item.component";
 import {MetricSelectionItemComponent} from "./add-metric-page/metric-selection-item/metric-selection-item.component";
-import {StatCardComponent} from "./card/stat-card.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {RegisterCardComponent} from "./auth/register/registerCard.component";
 import {LoginCardComponent} from "./auth/login/loginCard.component";
 import {ToastComponent} from "./toasts/toast.component";
 import {ProfileComponent} from "./profile/profile.component";
+import {ApplicationStatCardComponent} from "./stat-card/application-stat-card.component";
+import {StatCardComponent} from "./card/stat-card.component";
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import {ProfileComponent} from "./profile/profile.component";
     MetricSelectionItemComponent,
     ToastComponent,
     MetricSelectionItemComponent,
-    ProfileComponent
+    ProfileComponent,
+    ApplicationStatCardComponent
   ],
     imports: [
         BrowserModule,
---
+++ Vital-Frontend/src/app/stat-card/application-stat-card.component.ts
@@ -0,0 +1,43 @@
+import {Component, Input, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'app-stat-card',
+  template: `
+      <div [ngClass]="getClassList()">
+          <div class="card-body items-center justify-center">
+              <div class="flex flex-col items-center justify-center">
+                  <div class="text-sm text-slate-500 text-center">{{ title }}</div>
+                  <div class="text-3xl sm:text-4xl font-extrabold whitespace-nowrap"
+                       [class.text-bg-accent]="accentFont">{{ value }} {{ unit }}
+                  </div>
+              </div>
+          </div>
+      </div>
+  `,
+})
+export class ApplicationStatCardComponent implements OnInit {
+  @Input() title: string = '';
+  @Input() value: number = 0;
+  @Input() unit: string = '';
+  @Input() cardBackground: string = '';
+  @Input() accentFont: boolean = false;
+
+  classList: string[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.classList.push('card', 'card-compact', 'lg:card-normal', 'h-full', 'w-full', 'shadow');
+
+    if (this.cardBackground) {
+      this.classList.push(this.cardBackground);
+    } else {
+      this.classList.push('bg-base-100');
+    }
+  }
+
+  getClassList() {
+    return this.classList.join(' ');
+  }
+}
