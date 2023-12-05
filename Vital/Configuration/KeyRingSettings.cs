f9e5e1c2fc41662420d22c0018b8e0e690f62829
Matej Mazúr <matejmazur@outlook.com>
2023-12-05 10:30:15 +0100

    Add custom encryption for user data protection
    Added CustomLookupProtector and CustomLookupProtectorKeyRing classes for encryption and decryption of user data with symmetric key algorithm. Updated IdentityBuilder to use the new classes for protecting personal data. Additional keys and settings are added in appsettings files for different environments to initialize the key ring settings. Include these changes to enhance data security measures in the application.

---
+++ Vital/Configuration/KeyRingSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Vital.Configuration;
+
+public class KeyRingSettings
+{
+    public string Key { get; set; }
+    public string R { get; set; }
+    public string G { get; set; }
+    public string B { get; set; }
+}
